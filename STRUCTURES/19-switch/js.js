ca9275d92188fa56074bdafa92a41fed7a654269
Lucas Cordeiro <lucascordeirodelima12@gmail.com>
2025-01-27 17:47:58 -0300

    aula 19 switch

---
+++ STRUCTURES/19-switch/js.js
@@ -0,0 +1,13 @@
+let nome = "Lucas";
+
+switch (nome){
+    case "Lucas":
+        console.log("O nome é Lucas");
+        break;
+    case "João":
+        console.log("O nome é João");
+        break;
+    default:
+        console.log("O nome não foi encontrado");
+        break;
+}
