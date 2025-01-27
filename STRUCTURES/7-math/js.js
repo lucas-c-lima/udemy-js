385a4a43ba05f95ed1e48e37d709503da324e54c
Lucas Cordeiro <lucascordeirodelima12@gmail.com>
2025-01-27 11:00:58 -0300

    aula 7 math

---
+++ STRUCTURES/7-math/js.js
@@ -0,0 +1,19 @@
+let maior = Math.max(6, 12, 28, 55, 8);
+
+console.log(maior);
+
+let menor = Math.min(6, 12, 28, 55, 8);
+
+console.log(menor);
+
+let arredondar = Math.round(6, 12, 28, 55, 8);
+
+console.log(arredondar);
+
+let arredondarCima = Math.ceil(6, 12, 28, 55, 8);
+
+console.log(arredondarCima);
+
+let arredondarBaixo = Math.floor(6, 12, 28, 55, 8);
+
+console.log(arredondarBaixo);
