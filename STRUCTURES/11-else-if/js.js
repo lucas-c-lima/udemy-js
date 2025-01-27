f38558d00f4d90281cdc49156f287e3007d38f88
Lucas Cordeiro <lucascordeirodelima12@gmail.com>
2025-01-27 17:09:16 -0300

    aula 11 else if

---
+++ STRUCTURES/11-else-if/js.js
@@ -0,0 +1,16 @@
+let nome = "Lucas";
+let idade = 28;
+
+if(nome != undefined && nome == "João"){
+    console.log("Nome está definido")
+} else if (nome == "Lucas" && nome.length > 4 && idade == 50){
+    console.log("O nome é Lucas")
+} else {
+    console.log("Não é o Lucas!")
+}
+
+if(1 > 2){
+    console.log("Teste")
+} else if (1 == 1){
+    console.log("Testando")
+}
