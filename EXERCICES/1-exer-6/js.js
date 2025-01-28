1bfa9b3fb9cfa334d8bc8a331969f0983b8e8608
Lucas Cordeiro <lucascordeirodelima12@gmail.com>
2025-01-28 09:53:32 -0300

    exercices

---
+++ EXERCICES/1-exer-6/js.js
@@ -0,0 +1,7 @@
+let nome = "Lucas";
+let idade = 22;
+let cnh = true;
+
+console.log(typeof nome);
+console.log(typeof idade);
+console.log(typeof cnh);
---
+++ EXERCICES/3-exer-8/js.js
@@ -0,0 +1,5 @@
+const nome = "Lucas";
+
+if(nome == "Lucas"){
+    console.log(`Olá ${nome}, tudo bem?`)
+}
