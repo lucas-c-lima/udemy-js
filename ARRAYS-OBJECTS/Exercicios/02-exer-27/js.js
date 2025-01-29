fde31ce5f70145bf90922f0c99d832de78c85c0a
Lucas Cordeiro <lucascordeirodelima12@gmail.com>
2025-01-29 10:03:44 -0300

    exercicios arrays e objetos

---
+++ ARRAYS-OBJECTS/Exercicios/02-exer-27/js.js
@@ -0,0 +1,5 @@
+let nomes = ["Lucas", "João"]
+let testes = [true, false, true, true]
+
+console.log(nomes.length)
+console.log(testes.length)
---
+++ ARRAYS-OBJECTS/Exercicios/05-exer-30/js.js
@@ -0,0 +1,7 @@
+let names = ["João", "Maria", "José", "Lucas", "Rodrigo"]
+
+if(names.includes("Salete")){
+    console.log("O nome foi encontrado")
+} else {
+    console.log("O nome não foi encontrado")
+}
