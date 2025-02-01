5b16ccdfe8b6350ed9bc4cad8b0887cf3faf7e7f
Lucas Cordeiro <lucascordeirodelima12@gmail.com>
2025-02-01 20:54:25 -0300

    aulas events

---
+++ EVENTS/09-evento-scroll/scripts.js
@@ -0,0 +1,7 @@
+window.addEventListener("scroll", function(e){
+
+    if(this.window.scrollY > 1000){
+        console.log("Ativou")
+    }
+
+})
---
+++ EVENTS/11-evento-load/scripts.js
@@ -0,0 +1,8 @@
+// window.addEventListener("load", function(){
+//     alert("Assine os nossos termos de uso")
+// })
+
+window.addEventListener("beforeunload", function(e){
+    e.returnValue = null
+})
+
